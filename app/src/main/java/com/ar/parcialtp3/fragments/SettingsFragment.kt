bf756eccef87d2bd91424b3d67957127a7799f26
Flvckz <72632648+flvckz@users.noreply.github.com>
2023-10-31 23:32:07 -0300

    Feat/conexiones navgraph (#2)
    * agregado navgraph y paquetes
    
    * agregados todos los fragments al package y al navgraph

---
+++ app/src/main/java/com/ar/parcialtp3/fragments/SettingsFragment.kt
@@ -0,0 +1,21 @@
+package com.ar.parcialtp3.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ar.parcialtp3.R
+
+class SettingsFragment : Fragment() {
+
+    lateinit var v: View
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_settings, container, false)
+        return v
+    }
+}
