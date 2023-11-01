1fbfd418391dd3ae594278f76dbd501794f4a03d
flerner <felix.lerner01@gmail.com>
2023-11-01 18:56:13 -0300

    feat: spinner de provincias seteado

--- app/src/main/java/com/ar/parcialtp3/fragments/PublishFragment.kt
+++ app/src/main/java/com/ar/parcialtp3/fragments/PublishFragment.kt
@@ -1,17 +1,23 @@
 package com.ar.parcialtp3.fragments
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import com.ar.parcialtp3.R
+import com.ar.parcialtp3.entities.Provinces
 
 class PublishFragment : Fragment() {
 
@@ -32,6 +38,19 @@ class PublishFragment : Fragment() {
 
     //Text View
     lateinit var txtPhotoErr: TextView
+
+    //Spinners
+    lateinit var spnProvinces: Spinner
+    lateinit var provincesAdapter: ArrayAdapter<String>
+    var provincesList: List<String> = Provinces().getList()
+
+    //Misc
+    lateinit var selectedProvince: String
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        provincesAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, provincesList)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,6 +70,11 @@ class PublishFragment : Fragment() {
         btnAddPhoto = v.findViewById(R.id.btnAddPhoto)
 
         //TextView initialization
+
+        //Spinners initialization
+        spnProvinces = v.findViewById(R.id.spnLocation)
+
+        setUpSpinner(spnProvinces, provincesAdapter)
         return v
     }
 
@@ -71,5 +95,31 @@ class PublishFragment : Fragment() {
             }
         }
 
+        btnPubish.setOnClickListener{
+
+        }
+
+    }
+
+    private fun setUpSpinner(spinner: Spinner, adapter: ArrayAdapter<String>) {
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                when (spinner) {
+                    spnProvinces -> selectedProvince = provincesList[position]
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                showDialog()
+            }
+        }
+    }
+    private fun showDialog() {
+        val dialog = AlertDialog.Builder(context).setTitle("Error").setMessage("ERROR SPINNER")
+            .setCancelable(true).create()
+        dialog.show()
     }
 }
