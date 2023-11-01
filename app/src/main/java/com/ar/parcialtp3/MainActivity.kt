3c51edab1a116e3d2402e3f5b86fb5829e04f399
Felipe Fluck <felipe@fluck.com.ar>
2023-11-01 19:38:00 -0300

    implementacion parcial en main activity

--- app/src/main/java/com/ar/parcialtp3/MainActivity.kt
+++ app/src/main/java/com/ar/parcialtp3/MainActivity.kt
@@ -2,8 +2,13 @@ package com.ar.parcialtp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
