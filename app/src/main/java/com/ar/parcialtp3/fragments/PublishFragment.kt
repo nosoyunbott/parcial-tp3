af6d90695dd85fc84675fead4e5310c89fc1bbce
flerner <felix.lerner01@gmail.com>
2023-11-01 18:08:52 -0300

    feat: se agregaron campos faltantes

--- app/src/main/java/com/ar/parcialtp3/fragments/PublishFragment.kt
+++ app/src/main/java/com/ar/parcialtp3/fragments/PublishFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.ar.parcialtp3.R
 
 class PublishFragment : Fragment() {
 
     lateinit var v: View
+    lateinit var edtAge: EditText
+    lateinit var edtName: EditText
+    lateinit var edtDescription: EditText
+    lateinit var edtObservation: EditText
+    lateinit var edtWeight: EditText
+    lateinit var edtPhotos: EditText
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_publish, container, false)
+        edtAge = v.findViewById(R.id.edtAge)
+        edtName = v.findViewById(R.id.edtName)
         return v
     }
 }
