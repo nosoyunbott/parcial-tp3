fd0daf74aacca08abc9df94b34a262605b3661a2
Felipe Fluck <felipe@fluck.com.ar>
2023-11-02 15:07:48 -0300

    carrousel funcional

---
+++ app/src/main/java/com/ar/parcialtp3/adapters/ImageAdapter.kt
@@ -0,0 +1,40 @@
+package com.ar.parcialtp3.adapters
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.ar.parcialtp3.R
+
+class ImageAdapter(private val imageList: ArrayList<Int>, private val viewPager2: ViewPager2) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imgCarrouselContainer)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_container, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageList[position])
+        if (position == imageList.size-1) {
+            viewPager2.post(runnable)
+        }
+    }
+
+    private val runnable = Runnable {
+        imageList.addAll(imageList)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/ar/parcialtp3/fragments/DetailFragment.kt
+++ app/src/main/java/com/ar/parcialtp3/fragments/DetailFragment.kt
@@ -1,15 +1,29 @@
 package com.ar.parcialtp3.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.ar.parcialtp3.R
+import com.ar.parcialtp3.adapters.ImageAdapter
+import kotlin.math.abs
 
 class DetailFragment : Fragment() {
 
-    lateinit var v: View
+    private lateinit var v: View
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler: Handler
+    private lateinit var imageList: ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +31,60 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_detail, container, false)
+        //TODO lateinit var para db
+        init()
+        setUpTransformer()
+
+        viewPager2.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, 2000)
+            }
+        })
         return v
+    }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer() {
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r + 0.14f
+        }
+
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    private fun init() {
+        viewPager2 = v.findViewById(R.id.imgCarrousel)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.fav_icon)
+        imageList.add(R.drawable.home_icon)
+        imageList.add(R.drawable.adoption_icon)
+
+        adapter = ImageAdapter(imageList, viewPager2)
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
     }
 }
