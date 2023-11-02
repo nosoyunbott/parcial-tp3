6fe84698ba42e7347b57d302bf2a5b0179e2ae61
Felipe Fluck <felipe@fluck.com.ar>
2023-11-02 15:35:07 -0300

    added carrousel, working on images from db

--- app/src/main/java/com/ar/parcialtp3/fragments/DetailFragment.kt
+++ app/src/main/java/com/ar/parcialtp3/fragments/DetailFragment.kt
@@ -3,6 +3,7 @@ package com.ar.parcialtp3.fragments
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,9 @@ import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.ar.parcialtp3.R
 import com.ar.parcialtp3.adapters.ImageAdapter
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlin.math.abs
 
 class DetailFragment : Fragment() {
@@ -24,6 +28,8 @@ class DetailFragment : Fragment() {
     private lateinit var handler: Handler
     private lateinit var imageList: ArrayList<Int>
     private lateinit var adapter: ImageAdapter
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -68,7 +74,7 @@ class DetailFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        handler.postDelayed(runnable, 2000)
+        handler.postDelayed(runnable, 10000)
     }
 
     private fun init() {
@@ -76,9 +82,31 @@ class DetailFragment : Fragment() {
         handler = Handler(Looper.myLooper()!!)
         imageList = ArrayList()
 
-        imageList.add(R.drawable.fav_icon)
-        imageList.add(R.drawable.home_icon)
-        imageList.add(R.drawable.adoption_icon)
+        val documentId = "KPuDeYxakHBkM91fjQ7D"
+
+        db.collection("Publications").document(documentId).collection("dog")
+            .get()
+            .addOnCompleteListener { querySnapshot ->
+                if (querySnapshot.isSuccessful) {
+                    for (document in querySnapshot.result!!) {
+                        // Access the array of images inside the 'dogs' collection
+                        val imagesArray = document.getString("images") as? ArrayList<String>
+                        if (imagesArray != null) {
+                            // Iterate through the imagesArray and do something with the images
+                            for (i in 0 until imagesArray.size) {
+                                imageList.add(imagesArray[i].toInt())
+                                Log.d("Image URL", imagesArray[i])
+                            }
+                        } else {
+                            // Handle the case where 'images' field is not present or not an ArrayList<String>
+                            Log.e("Error", "Images field not found or not of the correct type")
+                        }
+                    }
+                } else {
+                    // Handle errors
+                    Log.e("Error", "Error getting documents: ${querySnapshot.exception}")
+                }
+            }
 
         adapter = ImageAdapter(imageList, viewPager2)
         viewPager2.adapter = adapter
