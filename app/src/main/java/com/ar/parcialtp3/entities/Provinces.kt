269574b0f6f80ab6a16d5293d4028a4c0e442a38
flerner <112443678+flerner@users.noreply.github.com>
2023-11-01 22:56:18 -0300

    Feat/creacion publicacion (#7)
    * feat: xml de registro
    
    * feat: se agregaron campos faltantes
    
    * feat: seteo de referencias a los componentes
    
    * feat: agregado funcionalidad multiples imagenes
    
    * feat: spinner de provincias seteado
    
    * feat: pegada a la api exitosa
    
    * feat: extraccion de la pegada de api a un servicio
    
    * feat: agregado de servicios para pegar a la api
    
    * feat: se agrego la query para traer todas las razas con sus subrazas
    
    * feat: populado de spinner de razas funcionando
    
    * feat: touppercase()

---
+++ app/src/main/java/com/ar/parcialtp3/entities/Provinces.kt
@@ -0,0 +1,35 @@
+package com.ar.parcialtp3.entities
+
+class Provinces {
+        val provincesList = listOf(
+            "Buenos Aires",
+            "CABA",
+            "Catamarca",
+            "Chaco",
+            "Chubut",
+            "Córdoba",
+            "Corrientes",
+            "Entre Ríos",
+            "Formosa",
+            "Jujuy",
+            "La Pampa",
+            "La Rioja",
+            "Mendoza",
+            "Misiones",
+            "Neuquén",
+            "Río Negro",
+            "Salta",
+            "San Juan",
+            "San Luis",
+            "Santa Cruz",
+            "Santa Fe",
+            "Santiago del Estero",
+            "Tierra del Fuego",
+            "Tucumán"
+        )
+
+        public fun getList(): List<String> {
+            return provincesList
+        }
+
+}
