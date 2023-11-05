f8755dc0c96760151312240acaef3282732ff158
Agustín Moragues <amoragues@provinciamicrocreditos.com>
2023-11-05 17:41:04 -0300

    code of autosuggestion

--- app/src/main/java/com/ar/parcialtp3/fragments/HomeFragment.kt
+++ app/src/main/java/com/ar/parcialtp3/fragments/HomeFragment.kt
@@ -2,14 +2,19 @@ package com.ar.parcialtp3.fragments
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.text.Editable
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.TextWatcher
 import android.text.style.ImageSpan
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.navigation.findNavController
@@ -17,11 +22,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ar.parcialtp3.R
 import com.ar.parcialtp3.adapters.CardAdapter
+import com.ar.parcialtp3.domain.Breed
 import com.ar.parcialtp3.domain.Card
 import com.ar.parcialtp3.entities.PublicationEntity
 import com.ar.parcialtp3.listener.OnViewItemClickedListener
+import com.ar.parcialtp3.services.DogDataService
 import com.ar.parcialtp3.services.firebase.FirebaseService
 import com.ar.parcialtp3.utils.SharedPrefUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment(), OnViewItemClickedListener {
@@ -29,9 +39,14 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
     lateinit var v: View
 
 
+    // Initialize a handler for debouncing user input
+
     private val firebaseService = FirebaseService()
     private lateinit var publications: List<PublicationEntity>
-
+    private lateinit var searchEditText: AutoCompleteTextView
+    private lateinit var suggestionAdapter: ArrayAdapter<String>
+    private lateinit var dogServiceAPI: DogDataService
+    private val handler = Handler(Looper.getMainLooper())
     private lateinit var filterContainer: LinearLayout
 
     private lateinit var recCardList: RecyclerView
@@ -50,6 +65,46 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
 
         activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
         activity?.actionBar?.setHomeButtonEnabled(true)
+
+
+        // Find the AutoCompleteTextView by ID
+        searchEditText = v.findViewById(R.id.searchEditText)
+
+        // Initialize and set up the suggestion adapter
+        suggestionAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line)
+        searchEditText.setAdapter(suggestionAdapter)
+
+
+        // Set an item click listener for handling selection
+        searchEditText.setOnItemClickListener { parent, view, position, id ->
+            val selectedSuggestion = parent.getItemAtPosition(position).toString()
+            // Handle the selected suggestion (e.g., perform a search with the selected value)
+        }
+
+
+        // Set a text change listener for debouncing and fetching suggestions
+        searchEditText.addTextChangedListener(object : TextWatcher {
+
+
+            override fun afterTextChanged(s: Editable?) {
+                // Implement your debouncing logic here
+                handler.removeCallbacksAndMessages(null)
+                handler.postDelayed({
+                    // This code will be executed after a delay (e.g., 500ms) of no input
+                    val input = s.toString()
+                    CoroutineScope(Dispatchers.Main).launch {
+                        fetchSuggestions(input)
+                    }
+                }, 500) // Adjust the delay time as needed
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                return;
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                return;
+            }
+        })
 
         return v
     }
@@ -97,6 +152,38 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
         }
         Handler().postDelayed({ refreshRecyclerView() }, 2500)
         //TODO Mejorar esto a una coroutina porque me crasheo un par de veces, lo tuve que pasar a 2500ms
+    }
+
+
+    // Function to fetch suggestions based on user input
+    private suspend fun fetchSuggestions(input: String) {
+        // Check if suggestions for the input are available in the cache
+        val cachedSuggestions = getCachedSuggestions(input)
+        if (cachedSuggestions != null) {
+            // Suggestions found in cache; update the adapter
+            suggestionAdapter.clear()
+            suggestionAdapter.addAll(cachedSuggestions)
+            suggestionAdapter.notifyDataSetChanged()
+        } else {
+            // Suggestions not found in cache; fetch data from the API
+
+            val breeds:List<Breed> = dogServiceAPI.getAllBreeds()
+            val breedList = breeds.map { it.name }
+            suggestionAdapter.clear()
+            suggestionAdapter.addAll(breedList)
+            suggestionAdapter.notifyDataSetChanged()
+
+            // Make an API request based on the user's input
+            // Cache the API response for future use
+            // Update the suggestionAdapter with the new suggestions
+        }
+    }
+
+    // Function to retrieve cached suggestions
+    private fun getCachedSuggestions(input: String): List<String>? {
+        // Implement your caching logic here
+        // Return cached suggestions if available, or null if not found
+        return null
     }
 
     private fun refreshRecyclerView() {
