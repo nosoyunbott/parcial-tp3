14780edee802aa3ada0d4a6bc8f8fe171950663c
juanbultri <juanbultri8@gmail.com>
2023-11-05 20:43:17 -0300

    splash-test

--- app/src/main/java/com/ar/parcialtp3/SplashActivity.kt
+++ app/src/main/java/com/ar/parcialtp3/SplashActivity.kt
@@ -3,28 +3,42 @@ package com.ar.parcialtp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.content.Intent;
-import com.ar.parcialtp3.fragments.PreStartFragment
+import android.widget.Button
+import com.ar.parcialtp3.fragments.StartFragment
 
 
 class SplashActivity : AppCompatActivity() {
 
-
+    lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed({
-            startActivity(Intent(this@SplashActivity, StartActivity::class.java))
-        }, 2500)
+        btn = findViewById(R.id.splash_btn)
+
+        btn.setOnClickListener {
+
+            // Inside your Activity class
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            val fragment = StartFragment() // Instantiate your Fragment here
+
+            // Replace the existing fragment with the new fragment
+            fragmentTransaction.replace(R.id.layoutLogin, fragment) // R.id.fragment_container is the ID of the container in your XML layout
+            fragmentTransaction.addToBackStack(null) // Optional: This allows the user to navigate back to the previous fragment
+            fragmentTransaction.commit()
 
     }
 
-    override fun onPause() {
-        super.onPause()
-        finish()
-    }
+
+}
+
+override fun onPause() {
+    super.onPause()
+    finish()
+}
 
 
 
