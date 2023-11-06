b9b74ec05997af402a36031e356d6af8ae645187
tomidamico <tomidamico@hotmail.com>
2023-11-06 02:44:02 -0300

    Se agrega funcionalidad search bar

--- app/src/main/java/com/ar/parcialtp3/fragments/HomeFragment.kt
+++ app/src/main/java/com/ar/parcialtp3/fragments/HomeFragment.kt
@@ -1,31 +1,44 @@
 package com.ar.parcialtp3.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.text.Editable
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.TextWatcher
 import android.text.style.ImageSpan
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.PopupMenu
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ar.parcialtp3.R
 import com.ar.parcialtp3.adapters.CardAdapter
+import com.ar.parcialtp3.domain.Breed
 import com.ar.parcialtp3.domain.Card
 import com.ar.parcialtp3.domain.Provinces
 import com.ar.parcialtp3.entities.PublicationEntity
 import com.ar.parcialtp3.listener.OnViewItemClickedListener
+import com.ar.parcialtp3.services.DogDataService
 import com.ar.parcialtp3.services.firebase.FirebaseService
 import com.ar.parcialtp3.utils.SharedPrefUtils
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment(), OnViewItemClickedListener {
@@ -34,6 +47,7 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
 
 
     private val firebaseService = FirebaseService()
+    private val dogDataService = DogDataService()
     private lateinit var publications: List<PublicationEntity>
 
     private lateinit var filterContainer: LinearLayout
@@ -47,11 +61,20 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
     private lateinit var clearFiltersTextView: TextView
     private lateinit var createDateTextView: TextView
     private lateinit var provinceTextView: TextView
+    private lateinit var searchEditText: AutoCompleteTextView
+    private lateinit var searchButton: ImageView
 
-    private lateinit var breedFilter : String
-    private lateinit var provinceFilter : String
-    private lateinit var filteredList : MutableList<Card>
+    private lateinit var allBreeds: MutableSet<String>
+    private lateinit var breeds: MutableSet<String>
+    private lateinit var subBreeds: MutableSet<String>
+    private lateinit var subBreedFilter: String
+    private lateinit var breedFilter: String
+    private lateinit var provinceFilter: String
+    private lateinit var filteredList: MutableList<Card>
+    private lateinit var suggestionsAdapter: ArrayAdapter<String>
+    private var selectedButton: Button? = null
     private var isAsc = true
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -65,6 +88,9 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
         clearFiltersTextView = v.findViewById(R.id.clearFiltersTextView)
         createDateTextView = v.findViewById(R.id.createDateTextView)
         provinceTextView = v.findViewById(R.id.provinceTextView)
+        searchEditText = v.findViewById(R.id.searchEditText)
+        searchEditText.threshold = 1
+        searchButton = v.findViewById(R.id.searchButton)
 
         activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
         activity?.actionBar?.setHomeButtonEnabled(true)
@@ -77,9 +103,17 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
         recCardList.setHasFixedSize(true)
         linearLayoutManager = LinearLayoutManager(context)
         recCardList.layoutManager = linearLayoutManager
+        allBreeds = mutableSetOf()
+        breeds = mutableSetOf()
+        subBreeds = mutableSetOf()
+        lifecycleScope.launch { fetchBreeds()}
+        suggestionsAdapter =
+            ArrayAdapter<String>(requireContext(), android.R.layout.simple_dropdown_item_1line)
+        searchEditText.setAdapter(suggestionsAdapter)
 
         provinceFilter = ""
         breedFilter = ""
+        subBreedFilter = ""
         filteredList = mutableListOf()
 
         cardListAdapter = CardAdapter(cardList, this, onClickFavourite = { id, _ ->
@@ -109,10 +143,10 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
                                 d.id,
                                 publication.location,
                                 publication.dog.adopted,
-                                d.getDate("timestamp")!!,
+                                d.getDate("createDate")!!,
                                 publication.dog.images[0]
                             )
-                            Log.d("Alo",dog.createDate.toString())
+                            Log.d("Alo", dog.createDate.toString())
                             cardList.add(dog)
                         }
                     }
@@ -134,21 +168,60 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
                 provinceFilter = menuItem.title.toString()
                 provinceTextView.text = provinceFilter
                 provinceTextView.visibility = View.VISIBLE
-                filterCardList(provinceFilter, breedFilter)
+                filterCardList(provinceFilter, breedFilter, subBreedFilter)
                 true
             }
 
             popupMenu.show()
         }
 
-        createDateTextView.setOnClickListener{
+        createDateTextView.setOnClickListener {
             orderCardList()
         }
+
+        searchEditText.setOnItemClickListener { parent, _, position, _ ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            performSearch(selectedItem)
+            hideKeyboard()
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val query = s.toString()
+                showSuggestions(query)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun showSuggestions(query: String) {
+        val filteredSuggestions = allBreeds.filter { it.startsWith(query, ignoreCase = true) }
+        suggestionsAdapter.clear()
+        suggestionsAdapter.addAll(filteredSuggestions)
+    }
+
+    private fun performSearch(query: String) {
+        if (breeds.contains(query)){
+            breedFilter = query
+            subBreedFilter = ""
+        }else if(subBreeds.contains(query)){
+            subBreedFilter = query
+            breedFilter = ""
+        }
+        filterCardList(provinceFilter,breedFilter,subBreedFilter)
+
+        searchEditText.text.clear()
+
+        selectedButton?.setBackgroundResource(R.drawable.button_transparent)
+        selectedButton = null
     }
 
     private fun refreshRecyclerView() {
-
-        var selectedButton: Button? = null
 
         val breeds = mutableSetOf<String>()
         for (p in publications) {
@@ -184,7 +257,7 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
 
             btnFilter.setOnClickListener {
                 breedFilter = filterName
-                filterCardList(provinceFilter, breedFilter)
+                filterCardList(provinceFilter, breedFilter, subBreedFilter)
 
                 selectedButton?.setBackgroundResource(R.drawable.button_transparent)
 
@@ -196,29 +269,41 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
             clearFiltersTextView.setOnClickListener {
                 provinceFilter = ""
                 breedFilter = ""
+                subBreedFilter = ""
                 selectedButton?.setBackgroundResource(R.drawable.button_transparent)
-                cardListAdapter = CardAdapter(cardList, this, onClickFavourite = { id, position -> })
+                cardListAdapter =
+                    CardAdapter(cardList, this, onClickFavourite = { id, position -> })
                 recCardList.adapter = cardListAdapter
                 filteredList = cardList.toMutableList()
                 provinceTextView.visibility = View.GONE
+                searchEditText.text.clear()
             }
 
             filterContainer.addView(btnFilter)
         }
     }
 
-    private fun filterCardList(province: String, breed: String) {
+    private fun filterCardList(province: String, breed: String, subBreed: String) {
         filteredList = cardList.toMutableList()
-        if(province.isNotEmpty()){
-            filteredList = filteredList.filter { it.location == province } as MutableList
-            cardListAdapter = CardAdapter(filteredList, this, onClickFavourite = { id, position -> })
+        if (province.isNotEmpty()) {
+            filteredList = filteredList.filter { it.location.equals(province, ignoreCase = true) } as MutableList
+            cardListAdapter =
+                CardAdapter(filteredList, this, onClickFavourite = { id, position -> })
             recCardList.adapter = cardListAdapter
         }
-        if(breed.isNotEmpty()){
-            filteredList = filteredList.filter { it.breed == breed } as MutableList
-            cardListAdapter = CardAdapter(filteredList, this, onClickFavourite = { id, position -> })
+        if (breed.isNotEmpty()) {
+            filteredList = filteredList.filter { it.breed.equals(breed, ignoreCase = true) } as MutableList
+            cardListAdapter =
+                CardAdapter(filteredList, this, onClickFavourite = { id, position -> })
             recCardList.adapter = cardListAdapter
         }
+        if (subBreed.isNotEmpty()) {
+            filteredList = filteredList.filter { it.subBreed.equals(subBreed, ignoreCase = true) } as MutableList
+            cardListAdapter =
+                CardAdapter(filteredList, this, onClickFavourite = { id, position -> })
+            recCardList.adapter = cardListAdapter
+        }
+        cardListAdapter.notifyDataSetChanged()
     }
 
     override fun onViewItemDetail(card: Card) {
@@ -227,19 +312,37 @@ class HomeFragment : Fragment(), OnViewItemClickedListener {
         navController.navigate(action)
     }
 
-    private fun orderCardList(){
-        if(filteredList.isEmpty()){
+    private fun orderCardList() {
+        if (filteredList.isEmpty()) {
             filteredList = cardList.toMutableList()
         }
-        if(isAsc){
+        if (isAsc) {
             filteredList.sortBy { it.createDate }
             isAsc = false
-        }else{
+        } else {
             filteredList.sortByDescending { it.createDate }
             isAsc = true
         }
         cardListAdapter = CardAdapter(filteredList, this, onClickFavourite = { id, position -> })
         recCardList.adapter = cardListAdapter
         cardListAdapter.notifyDataSetChanged()
+    }
+
+    private fun hideKeyboard() {
+        val imm =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+    }
+
+    private suspend fun fetchBreeds() {
+
+        val apiBreeds = dogDataService.getAllBreeds()
+
+        for (breed in apiBreeds) {
+            allBreeds.add(breed.name)
+            allBreeds.addAll(breed.subBreeds)
+            breeds.add(breed.name)
+            subBreeds.addAll(breed.subBreeds)
+        }
     }
 }
